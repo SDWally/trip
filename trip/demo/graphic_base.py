1c6482b7f337f977878ccdd9d49489635f1811b7
SDWally <1161512420@qq.com>
2023-10-21 01:35:56 +0800

    add: u

---
+++ trip/demo/graphic_base.py
@@ -0,0 +1,11 @@
+import time
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+pygame.draw.rect(screen, (255, 0, 34), pygame.Rect(42, 15, 40, 32))
+pygame.display.flip()
+
+
+time.sleep(10)
